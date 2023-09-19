2c6bbbf26338329ac332bbab992461af1fceaa56
Yameen Malik <65854545+YameenMalik@users.noreply.github.com>
2023-09-19 05:48:26 +0500

    Updated oracle price update script (#135)
    * cleaned the script
    
    * bumped to v0.1.57
    
    ---------
    
    Co-authored-by: YameenMalik <yameen@seed.im>

--- scripts/updateOraclePrice.ts
+++ scripts/updateOraclePrice.ts
@@ -5,7 +5,6 @@ import {
     getSignerFromSeed
 } from "../submodules/library-sui";
 import { OnChainCalls } from "../submodules/library-sui";
-import { deployProgram } from "../submodules/wormhole/testing/solana-test-validator/sdk-tests/helpers/utils";
 
 const deployment = readFile(DeploymentConfigs.filePath);
 
@@ -18,37 +17,11 @@ const ownerSigner = getSignerFromSeed(DeploymentConfigs.deployer, provider);
 const onChain = new OnChainCalls(ownerSigner, deployment);
 
 async function main() {
-    //only suitable for local
-    const pythObj = readFile("./pyth/priceInfoObject.json");
-    const pythPackage = readFile("./pythFakeDeployment.json");
-    const pythPackageId=deployment["markets"]["ETH-PERP"]["Objects"]["PriceOracle"]["dataType"].split("::")[0];
-    
-
     //calling function.
-    const result = await onChain.setOraclePrice({
-        price: 1000,
-        priceInfoFeedId:
-            pythObj["ETH-PERP"][process.env.DEPLOY_ON as string]["feed_id"],
-        pythPackageId: pythPackageId
+    await onChain.setOraclePrice({
+        price: 28000,
+        market: "BTC-PERP"
     });
-
-    //calling function.
-    const result2 = await onChain.setOraclePrice({
-        price: 1020,
-        priceInfoFeedId:
-            pythObj["BTC-PERP"][process.env.DEPLOY_ON as string]["feed_id"],
-        pythPackageId: pythPackage.objects.package.id
-    });
-
-    //calling function.
-    const result2 = await onChain.setOraclePrice({
-        price: 1020,
-        priceInfoFeedId:
-            pythObj["BTC-PERP"][process.env.DEPLOY_ON as string]["feed_id"],
-        pythPackageId: pythPackage.objects.package.id
-    });
-    console.log(result);
-    console.log(result2);
 }
 
 main();
